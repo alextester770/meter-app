4b532ada922e00691d9cec1a2c528b8dd5fd3ba1
alextester770 <alextester770@gmail.com>
2026-05-25 15:39:40 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 import pandas as pd
 
 app = Flask(__name__)
 
 EXCEL_FILE = "meters.xlsx"
 
-@app.route("/meter/<int:meter_id>", methods=["GET", "POST"])
+@app.route("/meter/<meter_id>", methods=["GET", "POST"])
 def meter_page(meter_id):
 
     df = pd.read_excel(EXCEL_FILE)
+
+    df["id"] = df["id"].fillna("").astype(str).str.strip()
+    meter_id = str(meter_id).strip()
 
     row = df[df["id"] == meter_id]
 
@@ -18,40 +21,88 @@ def meter_page(meter_id):
     index = row.index[0]
 
     if request.method == "POST":
+        current_raw = request.form.get("current")
+        
+        if current_raw is None or current_raw.strip() == "":
+            return "Ошибка: пустое значение"
         try:
             current_value = float(request.form.get("current"))
-        except:
-            return "Ошибка: введите число"
+            prev_value = float(df.at[index, "prev"])
 
+        # защита от уменьшения
+            if current_value < prev_value:
+                return "Ошибка: значение не может быть меньше предыдущего"
 
-        df.at[index, "current"] = current_value
+        # расчет расхода
+            diff = current_value - prev_value
 
-        df.to_excel(EXCEL_FILE, index=False)
+        # сохранение
+            df.at[index, "current"] = current_value
+            df.to_excel(EXCEL_FILE, index=False)
 
-        diff = float(current_value) - float(df.at[index, "prev"])
-
-        return f"""
+            return f"""
         <h1>Данные сохранены</h1>
         <p>Расход за месяц: {diff}</p>
         """
 
+        except:
+            return "Ошибка: введите число"
+
     data = row.iloc[0]
 
     return f"""
-    <h1>Счетчик {data['meter']}</h1>
+  <!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 
-    <p>ИИН: {data['iin']}</p>
-    <p>Контрагент: {data['name']}</p>
-    <p>Предыдущие показания: {data['prev']}</p>
+<body class="bg-light">
 
+<div class="container mt-3">
+
+  <div class="card shadow-sm p-3">
+    <h4 class="mb-3">Счетчик № {data['id']}</h4>
+
+    <p><b>ИИН:</b> {data['iin']}</p>
+    <p><b>Контрагент:</b> {data['name']}</p>
+    <p><b>Расположение:</b> {data['position']}</p>
+    <p><b>Предыдущие:</b> {data['prev']}</p>
+    <p><b>Текущий долг: </b> {data['dolg']}<b> на месяц: </b>{data['date']}</p>
     <form method="POST">
-        <input type="number" name="current" placeholder="Введите текущие показания" required>
-        <button type="submit">Сохранить</button>
+      <input class="form-control mb-2" type="number" name="current" placeholder="Текущие показания" required>
+      <button class="btn btn-primary w-100">Сохранить</button>
+    </form>
+
+  </div>
+
+</div>
+
+</body>
+</html>
+    """
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        file.save("meters.xlsx")
+        return "Файл загружен успешно"
+
+    return """
+    <h2>Загрузка Excel</h2>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit">Загрузить</button>
     </form>
     """
-
+@app.route("/download")
+def download():
+    return send_file("meters.xlsx", as_attachment=True)
+    
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    
